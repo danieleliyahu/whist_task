94c1e6909b307b255212e5d1e0bb72f31c9a10da
danieleliyahu <77384369+danieleliyahu@users.noreply.github.com>
2021-08-20 11:07:03 +0300

    first commit

--- back/index.js
+++ back/index.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const itemsRouter = require("./routers/itemsRouter");
+const dotenv = require("dotenv");
+
 const port = 5000;
 const app = express();
-
+dotenv.config();
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 mongoose.connect(
   process.env.MONGODB_URL ||
-    "mongodb+srv://danidani80:9925259@cluster0.rqrrd.mongodb.net/whistTask?retryWrites=true&w=majority",
+    `mongodb+srv://danidani80:${process.env.MONGODB_PAS}@cluster0.rqrrd.mongodb.net/whistTask?retryWrites=true&w=majority`,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
